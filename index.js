f8728f11ee0e64d295007841ee4003617d3687b0
Rishita Srivastava <rixsri.13@gmail.comgit config --global user.name Rishita>
2022-12-23 22:52:16 +0530

    toggle in all pages

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-let clock = () => {
-    let hrs = new Date().getHours();
-    let mins = new Date().getMinutes();
-    let secs = new Date().getSeconds();
-    let period = "AM";
-    if (hrs == 0) {
-        hrs = 12;
-    } 
-    else if (hrs >= 12) {
-        hrs = hrs - 12;
-        period = "PM";
-    }
-    hrs = hrs < 10 ? "0" + hrs : hrs;
-    mins = mins < 10 ? "0" + mins : mins;
-    secs = secs < 10 ? "0" + secs : secs;
+// let clock = () => {
+//     let hrs = new Date().getHours();
+//     let mins = new Date().getMinutes();
+//     let secs = new Date().getSeconds();
+//     let period = "AM";
+//     if (hrs == 0) {
+//         hrs = 12;
+//     } 
+//     else if (hrs >= 12) {
+//         hrs = hrs - 12;
+//         period = "PM";
+//     }
+//     hrs = hrs < 10 ? "0" + hrs : hrs;
+//     mins = mins < 10 ? "0" + mins : mins;
+//     secs = secs < 10 ? "0" + secs : secs;
 
-    let time = `${hrs}:${mins}:${secs} ${period}`;
-    document.getElementById("clock").innerText = time;
-    setTimeout(clock, 1000);
-};
-clock()
+//     let time = `${hrs}:${mins}:${secs} ${period}`;
+//     document.getElementById("clock").innerText = time;
+//     setTimeout(clock, 1000);
+// };
+// clock()
 
 $(document).ready(function(){
     $('#icon1').click(function(){
